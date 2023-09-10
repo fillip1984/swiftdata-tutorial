90b0b43bdd927cfb0dd683000cc861071ddcfa13
fillip1984 <fillip1984@gmail.com>
2023-09-10 12:28:30 -0400

    Initial Commit

---
+++ swiftdata-tutorial/swiftdata_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftdata_tutorialApp.swift
+//  swiftdata-tutorial
+//
+//  Created by Phillip Williams on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftdata_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
